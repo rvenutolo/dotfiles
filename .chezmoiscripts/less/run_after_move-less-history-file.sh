62741fa720a357fc6dc7d4815e874378c6d48ff4
Rick Venutolo <rick@venutolo.org>
2023-02-19 20:15:38 -0600

    add script to move less history file

---
+++ .chezmoiscripts/less/run_after_move-less-history-file.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +u
+readonly current_history_file="${LESSHISTFILE:-"${HOME}/.lesshst"}"
+source "${XDG_CONFIG_HOME}/bash/functions"
+source "${XDG_CONFIG_HOME}/bash/exports"
+set -u
+
+readonly new_history_file="${LESSHISTFILE}"
+mkdir --parents "$(dirname "${new_history_file}")"
+
+if [[ -f "${current_history_file}" && "${current_history_file}" != "${new_history_file}" ]]; then
+  if [[ "$(prompt_yn "Move ${current_history_file} -> ${new_history_file}?")" == 'y' ]]; then
+    log "Moving: ${current_history_file} -> ${new_history_file}"
+    mv "${current_history_file}" "${new_history_file}"
+    log "Moved: ${current_history_file} -> ${new_history_file}"
+  fi
+fi
