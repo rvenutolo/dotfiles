faf2dd515f54890695e451a586f1df8862c56f4b
Rick Venutolo <rick@venutolo.org>
2023-02-26 22:11:43 -0600

    loginctl linger

---
+++ .chezmoiscripts/loginctl/run_after_loginctl-linger.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +u
+source "${XDG_CONFIG_HOME}/bash/functions"
+source "${XDG_CONFIG_HOME}/bash/exports"
+set -u
+
+if [[ "$(loginctl show-user "${USER}" --property='Linger')" == 'Linger=yes' ]]; then
+  exit 0
+fi
+if ! prompt_yn 'Set user to linger?'; then
+  exit 0
+fi
+
+log 'Setting user to linger'
+sudo loginctl enable-linger "${USER}"
+log 'Set user to linger'
