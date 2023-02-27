dd010d703e904b5cbe9f599bf758a9d4c2327fde
Rick Venutolo <rick@venutolo.org>
2023-02-26 22:11:56 -0600

    rustup toolchain stable

---
+++ .chezmoiscripts/rustup/run_after_set-rustup-toolchain-stable.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +u
+source "${XDG_CONFIG_HOME}/bash/functions"
+source "${XDG_CONFIG_HOME}/bash/exports"
+set -u
+
+if ! exexcutable_exists 'rustup'; then
+  exit 0
+fi
+if [[ "$(rustup toolchain list)" == 'stable'* ]]; then
+  exit 0
+fi
+if ! prompt_yn 'Set rustup toolchain to stable?'; then
+  exit 0
+fi
+
+log 'Setting rustup toolchain to stable'
+rustup toolchain install 'stable'
+log 'Set rustup toolchain to stable'
