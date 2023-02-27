4ec0bbbc11415f1cf627b6193a405dc3ae0933db
Rick Venutolo <rick@venutolo.org>
2023-02-26 22:11:09 -0600

    add flathub repo

---
+++ .chezmoiscripts/flatpak/run_after_add-flathub-repo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +u
+source "${XDG_CONFIG_HOME}/bash/functions"
+source "${XDG_CONFIG_HOME}/bash/exports"
+set -u
+
+if ! executable_exists 'flatpak'; then
+  exit 0
+fi
+if flatpak remotes --columns=title | contains_word 'Flathub'; then
+  exit 0
+fi
+if ! prompt_yn 'Add Flathub flatpak repo?'; then
+  exit 0
+fi
+
+log 'Adding Flathub flatpak repo'
+flatpak remote-add --if-not-exists 'flathub' 'https://flathub.org/repo/flathub.flatpakrepo'
+log 'Added Flathub flatpak repo'
