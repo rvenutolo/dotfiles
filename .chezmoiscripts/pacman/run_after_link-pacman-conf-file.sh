6c1af1441220f1ded8aa57ef510ddfa7f1d2af0c
Rick Venutolo <rick@venutolo.org>
2023-02-26 22:10:49 -0600

    link pacman conf file

---
+++ .chezmoiscripts/pacman/run_after_link-pacman-conf-file.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set +u
+source "${XDG_CONFIG_HOME}/bash/functions"
+source "${XDG_CONFIG_HOME}/bash/exports"
+set -u
+
+readonly link_file='/etc/pacman.conf'
+readonly target_file="${XDG_CONFIG_HOME}/pacman/pacman.conf"
+
+if [[ -L "${link_file}" ]]; then
+  exit 0
+fi
+if [[ -f "${link_file}" ]]; then
+  if ! prompt_yn "${link_file} exists - Link: ${target_file} -> ${link_file}?"; then
+    exit 0
+  fi
+else
+  if ! prompt_yn "Link: ${target_file} -> ${link_file}?"; then
+    exit 0
+  fi
+fi
+
+log "Linking: ${target_file} -> ${link_file}"
+if [[ -f "${link_file}" ]]; then
+  sudo rm "${link_file}"
+fi
+sudo mkdir --parents "$(dirname "${link_file}")"
+sudo ln --symbolic "${target_file}" "${link_file}"
+log "Linked: ${target_file} -> ${link_file}"
