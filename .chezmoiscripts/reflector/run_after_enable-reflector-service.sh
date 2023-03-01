2d9cff62caec8fe95b1376f753e2c83d90b76f1d
Rick Venutolo <rick@venutolo.org>
2023-02-28 19:01:21 -0600

    move systemd service scripts to separate dirs based on relevant program

--- .chezmoiscripts/reflector/run_after_enable-reflector-service.sh
+++ .chezmoiscripts/reflector/run_after_enable-reflector-service.sh
@@ -5,21 +5,8 @@ set -euo pipefail
 source "${XDG_CONFIG_HOME}/bash/functions"
 source "${XDG_CONFIG_HOME}/bash/exports"
 
-if ! is_arch; then
-  exit 0
-fi
 if ! executable_exists 'reflector'; then
   exit 0
-fi
-
-if ! systemctl is-enabled --user --quiet 'reflector.timer' || ! systemctl is-active --user --quiet 'reflector.timer'; then
-  if prompt_yn 'Write: /etc/xdg/reflector/reflector.conf?'; then
-    log 'Writing: /etc/xdg/reflector/reflector.conf'
-    sudo sed --in-place '/^$/q' '/etc/xdg/reflector/reflector.conf'
-    echo -e '--country United States\n--age 12\n--protocol https\n--sort rate\n--save /etc/pacman.d/mirrorlist' |
-      sudo tee --append '/etc/xdg/reflector/reflector.conf' > /dev/null
-    log 'Wrote: /etc/xdg/reflector/reflector.conf'
-  fi
 fi
 
 if ! systemctl is-enabled --user --quiet 'reflector.timer' && prompt_yn 'Enable and start reflector services?'; then
