25b8bcfee6a938cf6bed8384b33836a9f381d887
Rick Venutolo <rick@venutolo.org>
2023-03-01 19:09:17 -0600

    un-template age decrypt script

--- .chezmoiscripts/run_before_decrypt-private-key.sh
+++ .chezmoiscripts/run_before_decrypt-private-key.sh
@@ -6,8 +6,7 @@ function log() {
   echo "${0##*/}: $1" >&2
 }
 
-readonly chezmoi_config_dir="$(dirname '{{ .chezmoi.configFile }}')"
-readonly age_key_file="${chezmoi_config_dir}/age_key.txt"
+readonly age_key_file="$(dirname "${CHEZMOI_CONFIG_FILE}")/age_key.txt"
 
 if [[ -f "${age_key_file}" ]]; then
   exit 0
@@ -23,6 +22,6 @@ if ! type -aPf age > /dev/null 2>&1; then
 fi
 
 log 'Decrypting age key'
-age --decrypt --output "${age_key_file}" "{{ .chezmoi.sourceDir }}/key.txt.age"
+age --decrypt --output "${age_key_file}" "${CHEZMOI_SOURCE_DIR}/key.txt.age"
 log 'Decrypted age key'
 chmod 600 "${age_key_file}"
