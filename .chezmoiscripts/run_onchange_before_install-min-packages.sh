93404a509fb3f12293b65ebfd81f2a6b720697f7
Rick Venutolo <rick@venutolo.org>
2023-04-09 14:49:47 -0500

    add alacritty

--- .chezmoiscripts/run_onchange_before_install-min-packages.sh
+++ .chezmoiscripts/run_onchange_before_install-min-packages.sh
@@ -12,11 +12,11 @@ function executable_exists() {
 }
 
 if executable_exists 'pacman'; then
-  sudo pacman --sync --refresh --needed --noconfirm bash-completion git openssh
+  sudo pacman --sync --refresh --needed --noconfirm alacritty bash-completion git openssh
 elif executable_exists 'dnf'; then
-  sudo dnf install --assumeyes bash-completion git openssh
+  sudo dnf install --assumeyes alacritty bash-completion git openssh
 elif executable_exists 'apt'; then
-  sudo apt install --assume-yes bash-completion git openssh-client
+  sudo apt install --assume-yes alacritty bash-completion git openssh-client
 else
   log 'Unable to determine package manager'
   exit 2
