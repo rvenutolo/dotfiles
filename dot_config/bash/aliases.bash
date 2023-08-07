557000723fae5db43dca35a8495ad1e778727765
Rick Venutolo <rick@venutolo.org>
2023-08-06 20:49:58 -0400

    re-add home-manager check

--- dot_config/bash/aliases.bash
+++ dot_config/bash/aliases.bash
@@ -115,12 +115,14 @@ alias m='micro'
 alias n='nano'
 
 # nix home-manager
-alias hm='home-manager'
-alias hmd='cd ${HOME_MANAGER_DIR}'
-alias hmgd='home-manager generations | head -n 2 | tac | cut -d " " -f 7 | xargs nix store diff-closures'
-alias hmp='home-manager packages'
-alias hms='home-manager switch --flake ${HOME_MANAGER_DIR}#${USER} && hmgd'
-alias hmu='nix flake update ${HOME_MANAGER_DIR} && hms'
+if __executable_exists 'home-manager'; then
+  alias hm='home-manager'
+  alias hmd='cd ${HOME_MANAGER_DIR}'
+  alias hmgd='home-manager generations | head -n 2 | tac | cut -d " " -f 7 | xargs nix store diff-closures'
+  alias hmp='home-manager packages'
+  alias hms='home-manager switch --flake ${HOME_MANAGER_DIR} && hmgd'
+  alias hmu='nix flake update ${HOME_MANAGER_DIR} && hms'
+fi
 
 # misc - shorter alias
 alias g='git'
