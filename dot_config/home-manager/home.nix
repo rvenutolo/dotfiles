038c176b503cf3126e8844a9c870aa96ec95db14
Rick Venutolo <rick@venutolo.org>
2023-08-07 21:55:39 -0400

    udpate nix stuff

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -1,19 +1,10 @@
 { config, pkgs, lib, ... }:
-
 {
-
-  home = {
-    username = "rvenutolo";
-    homeDirectory = "/home/rvenutolo";
-    stateVersion = "23.05";
-    packages = with builtins; with pkgs.lib; map (name: getAttrFromPath (splitString "." name) pkgs) (fromJSON (readFile ./pkgs.json));
-  };
-
-  nixpkgs.config = {
-    allowUnfree = true;
-    allowUnfreePredicate = _: true;
-  };
-
+  imports = [
+    ./user.nix
+    ./packages.nix
+    ./nixpkgs-config.nix
+  ];
+  home.stateVersion = "23.05";
   programs.home-manager.enable = true;
-
 }
