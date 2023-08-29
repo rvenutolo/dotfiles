2716a05f03d35962ac2a17bb62f6892d322fe4fe
Rick Venutolo <rick@venutolo.org>
2023-08-28 21:49:43 -0400

    add kubectl completion

--- dot_config/exact_bash/rc.bash
+++ dot_config/exact_bash/rc.bash
@@ -153,6 +153,7 @@ unset -v file
 __is_readable_file "${XDG_CONFIG_HOME}/dircolors" && eval "$(dircolors "${XDG_CONFIG_HOME}/dircolors")"
 __executable_exists 'aws_completer' && complete -C 'aws_completer' aws
 __executable_exists 'zoxide' && eval "$(zoxide init bash)"
+__executable_exists 'kubectl' && source <(kubectl completion bash)
 
 # add completions for aliases
 # sourced here, rather than earlier, to make sure all aliases and bash completions have been sourced
