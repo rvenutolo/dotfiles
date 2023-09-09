bf162346fb1248fe48caf33e2eac9ad4e5523db4
Rick Venutolo <rick@venutolo.org>
2023-09-08 21:30:46 -0400

    change flag

--- .chezmoiscripts/run_once_before_00-install-packages.sh
+++ .chezmoiscripts/run_once_before_00-install-packages.sh
@@ -24,13 +24,11 @@ function prompt_yn() {
 }
 
 if executable_exists 'apt-get'; then
-  sudo apt-get install --assume-yes age git openssh-client
+  sudo apt-get install --yes age git openssh-client
 elif executable_exists 'dnf'; then
   sudo dnf install --assumeyes age git openssh
 elif executable_exists 'pacman'; then
   sudo pacman --sync --refresh --needed --noconfirm age git openssh
-elif executable_exists 'zypper'; then
-  ## TODO fill in
 else
   log 'Unknown package manager - Please install age, git, and ssh'
   if ! prompt_yn 'Continue?'; then
