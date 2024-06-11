fe489f8d95a0b56ddf2b9f1933731223515bf01c
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-06-10 21:53:04 -0500

    update keys script

--- .chezmoiscripts/run_once_before_01-get-keys.sh
+++ .chezmoiscripts/run_once_before_01-get-keys.sh
@@ -15,6 +15,11 @@ function die() {
   exit 1
 }
 
+# $1 = file
+function is_readable_file() {
+  [[ -f "$1" && -r "$1" ]]
+}
+
 function auto_answer() {
   [[ "${SCRIPTS_AUTO_ANSWER:-}" == [Yy] ]]
 }
@@ -38,7 +43,7 @@ function prompt_ny() {
 
 # $1 = url
 function dl() {
-  curl --fail --silent --location "$1"
+  curl --disable --fail --silent --location --show-error "$@"
 }
 
 if ! executable_exists 'age'; then
@@ -52,24 +57,45 @@ fi
 keys_dir="${HOME}/.keys"
 mkdir --parents "${keys_dir}"
 
-age_key_file="${keys_dir}/age.key"
-age_key_contents="$(dl 'https://raw.githubusercontent.com/rvenutolo/crypt/main/keys/age.key')"
-if [[ -n "${age_key_content}" ]]; then
-  log "Decrypting: ${age_key_file}"
-  until age --decrypt --output "${age_key_file}" <<< "${age_key_contents}"; do :; done
+readonly age_key_file="${keys_dir}/age.key"
+
+if ! is_readable_file "${age_key_file}" || prompt_ny "${age_key_file} exists - Download again?"; then
+  log "Downloading: ${age_key_file}"
+  age_key_content="$(download 'https://raw.githubusercontent.com/rvenutolo/crypt/main/keys/age.key')"
+  if [[ -n "${age_key_content}" ]]; then
+    log "Decrypting: ${age_key_file}"
+    until age --decrypt --output "${age_key_file}" <<< "${age_key_content}"; do :; done
+  else
+    die "age key file content is empty"
+  fi
 fi
 
-for url in $(dl 'https://api.github.com/repos/rvenutolo/crypt/contents/keys' | jq -r '.[].download_url'); do
+function dl_key() {
+  log "Downloading: ${key_file}"
+  key_contents="$(download "${url}")"
+  if [[ -n "${key_contents}" ]]; then
+    log "Decrypting: ${key_file}"
+    age --decrypt --identity "${age_key_file}" --output "${key_file}" <<< "${key_contents}"
+  else
+    die "${filename} content is empty"
+  fi
+}
+
+for url in $(download 'https://api.github.com/repos/rvenutolo/crypt/contents/keys' | jq -r '.[].download_url'); do
   filename="$(basename "${url}")"
   if [[ "${filename}" == 'age.key' ]]; then
     continue
   fi
-  key_contents="$(dl "${url}")"
-  if [[ -n "${key_contents}" ]]; then
-    if [[ "${filename}" == "id_ed25519" ]] || prompt_ny "Decrypt ${filename}?"; then
-      key_file="${keys_dir}/${filename}"
-      log "Decrypting: ${key_file}"
-      age --decrypt --identity "${age_key_file}" --output "${key_file}" <<< "${key_contents}"
+  key_file="${keys_dir}/${filename}"
+  if is_readable_file "${key_file}"; then
+    if prompt_ny "${key_file} already exists - Download again?"; then
+      dl_key
+    fi
+  else
+    if [[ "${filename}" == 'id_ed25519' ]]; then
+      dl_key
+    elif prompt_ny "Download ${key_file}"; then
+      dl_key
     fi
   fi
 done
