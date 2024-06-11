b0540b48d8f25482ed2d3d89c5f4e452eb60bc58
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-06-10 19:49:51 -0500

    move home-manager state version to template

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -5,15 +5,6 @@
     ./packages.nix
     ./nixpkgs-config.nix
   ];
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See the Home
-  # Manager release notes for a list of state version changes in each release.
-  #
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
   targets.genericLinux.enable = true;
   xdg.mime.enable = true;
