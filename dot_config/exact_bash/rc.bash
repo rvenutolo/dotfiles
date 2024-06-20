d2df851ce74bad5ff1e1def011f21223961030cc
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-06-19 19:32:27 -0500

    add appman completion

--- dot_config/exact_bash/rc.bash
+++ dot_config/exact_bash/rc.bash
@@ -149,6 +149,7 @@ unset -v file
 # misc init stuff
 __is_readable_file "${XDG_CONFIG_HOME}/dircolors" && eval "$(dircolors "${XDG_CONFIG_HOME}/dircolors")"
 __executable_exists 'aws_completer' && complete -C 'aws_completer' aws
+__executable_exists 'appman' && complete -W "$(cat "${XDG_DATA_HOME}/appman/appman/list" 2> /dev/null)" appman
 __executable_exists 'zoxide' && eval "$(zoxide init bash)"
 __executable_exists 'kubectl' && source <(kubectl completion bash)
 
