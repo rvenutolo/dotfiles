b75bb68fad57d511b8b8c0a04c9ba329343b45c0
Rick Venutolo <rick@venutolo.org>
2024-06-25 21:42:32 -0500

    misc updates

--- .chezmoiscripts/run_before_50-sdkman-config-file.sh
+++ .chezmoiscripts/run_before_50-sdkman-config-file.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-function log() {
-  echo "${0##*/}: $*" >&2
+function die() {
+  echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
+  exit 1
 }
 
 if [[ -z "${SDKMAN_DIR:-}" ]]; then
@@ -24,10 +25,10 @@ readonly target_file="${XDG_CONFIG_HOME:-${HOME}/.config}/sdkman/config"
 
 if cmp --silent "${sdkman_config_file}" "${target_file}"; then
   # files have same content, so delete and let chezmoi symlink the file
+  log "Removing ${sdkman_config_file}"
   rm "${sdkman_config_file}"
 else
   # files have different content
-  log "${sdkman_config_file} and ${target_file} have different contents"
   diff --color "${sdkman_config_file}" "${target_file}"
-  exit 2
+  die "${sdkman_config_file} and ${target_file} have different contents"
 fi
--- .chezmoiscripts/run_once_before_00-install-packages.sh
+++ .chezmoiscripts/run_once_before_00-install-packages.sh
@@ -3,40 +3,31 @@
 set -euo pipefail
 
 function log() {
-  echo "${0##*/}: $*" >&2
+  echo -e "\033[0;32m[$(date +%T) ${0##*/}] $*\033[0m" >&2
+}
+
+function die() {
+  echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
+  exit 1
 }
 
 function executable_exists() {
   command -v "$1" > /dev/null 2>&1
 }
 
-function prompt_yn() {
-  REPLY=''
-  while [[ "${REPLY}" != 'y' && "${REPLY}" != 'n' ]]; do
-    read -rp "$1 [Y/n]: "
-    if [[ "${REPLY}" == '' || ${REPLY} == [yY] ]]; then
-      REPLY='y'
-    elif [[ "${REPLY}" == [nN] ]]; then
-      REPLY='n'
-    fi
-  done
-  [[ "${REPLY}" == 'y' ]]
-}
-
 if executable_exists 'apt'; then
+  log 'Updating packages with apt'
   sudo apt update
   sudo apt upgrade --yes
   sudo apt install --yes age curl git jq openssh-client
 elif executable_exists 'dnf'; then
+  log 'Updating packages with dnf'
   sudo dnf upgrade
   sudo dnf install --assumeyes age curl git jq openssh
 elif executable_exists 'pacman'; then
+  log 'Updating packages with pacman'
   sudo pacman --sync --refresh --sysupgrade
   sudo pacman --sync --refresh --needed --noconfirm age curl git jq openssh
 else
-  log 'Unknown package manager - Please install age, git, and ssh'
-  if ! prompt_yn 'Continue?'; then
-    log 'Exiting'
-    exit 2
-  fi
+  die 'Unknown package manager - Please install age, git, and ssh'
 fi
