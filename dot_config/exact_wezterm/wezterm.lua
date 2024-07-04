9d23892cd20c35032b668b450a395b74e1309e24
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-07-03 22:58:52 -0500

    more tab title stuff

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -38,50 +38,23 @@ local function has_unseen_output(tab)
   return false
 end
 
-local function get_color_scheme(scheme_name)
-  return wezterm.color.get_builtin_schemes()[scheme_name]
-end
-
-local function scheme_color(scheme_name, key)
-  local current_color_scheme = get_color_scheme(scheme_name)
-  local key_to_color = {
-    black = current_color_scheme.ansi[1],
-    red = current_color_scheme.ansi[2],
-    green = current_color_scheme.ansi[3],
-    yellow = current_color_scheme.ansi[4],
-    blue = current_color_scheme.ansi[5],
-    magenta = current_color_scheme.ansi[6],
-    cyan = current_color_scheme.ansi[7],
-    white = current_color_scheme.ansi[8],
-    black_bright = current_color_scheme.brights[1],
-    red_bright = current_color_scheme.brights[2],
-    green_bright = current_color_scheme.brights[3],
-    yellow_bright = current_color_scheme.brights[4],
-    blue_bright = current_color_scheme.brights[5],
-    magenta_bright = current_color_scheme.brights[6],
-    cyan_bright = current_color_scheme.brights[7],
-    white_bright = current_color_scheme.brights[8]
-  }
-  return key_to_color[key]
-end
-
 wezterm.on(
   "format-tab-title",
   function(tab, tabs, panes, config, hover, max_width)
     local tab_title = get_tab_title(tab)
     if tab.is_active then
       return {
-        { Foreground = { Color = scheme_color(config.color_scheme, "white") } },
+        { Foreground = { AnsiColor = "White" } },
         { Text = tab_title }
       }
     elseif has_unseen_output(tab) then
       return {
-        { Foreground = { Color = scheme_color(config.color_scheme, "green") } },
+        { Foreground = { AnsiColor = "Green" } },
         { Text = tab_title }
       }
     else
       return {
-        { Foreground = { Color = scheme_color(config.color_scheme, "blue") } },
+        { Foreground = { AnsiColor = "Blue" } },
         { Text = tab_title }
       }
     end
