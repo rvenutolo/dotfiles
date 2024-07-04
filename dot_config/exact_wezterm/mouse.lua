6e5c6f053d84fbbb824d8deac9cc989ebb29ec91
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-07-04 00:16:32 -0500

    disable mouse selection override

--- dot_config/exact_wezterm/mouse.lua
+++ dot_config/exact_wezterm/mouse.lua
@@ -2,18 +2,18 @@ local wezterm = require("wezterm")
 
 return {
   bindings = {
-    {
-      -- override default copy behavior with a no-op
-      event = { Up = { streak = 1, button = "Left" } },
-      mods = "NONE",
-      action = wezterm.action.Nop,
-    },
-    {
-      -- disable down event to avoid unexpected behavior
-      event = { Down = { streak = 1, button = "Left" } },
-      mods = "CTRL",
-      action = wezterm.action.Nop,
-    },
+    --{
+    --  -- override default copy behavior with a no-op
+    --  event = { Up = { streak = 1, button = "Left" } },
+    --  mods = "NONE",
+    --  action = wezterm.action.Nop,
+    --},
+    --{
+    --  -- disable down event to avoid unexpected behavior
+    --  event = { Down = { streak = 1, button = "Left" } },
+    --  mods = "CTRL",
+    --  action = wezterm.action.Nop,
+    --},
     {
       -- CTRL-click to open hyperlinks
       event = { Up = { streak = 1, button = "Left" } },
