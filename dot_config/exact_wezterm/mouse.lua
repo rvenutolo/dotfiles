6bde0a2795ff6593c3fa807cfc5d8b36c9bc4261
Rick Venutolo <rick@venutolo.org>
2024-07-04 22:46:05 -0500

    act

--- dot_config/exact_wezterm/mouse.lua
+++ dot_config/exact_wezterm/mouse.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 return {
   bindings = {
@@ -6,25 +7,25 @@ return {
     --  -- override default copy behavior with a no-op
     --  event = { Up = { streak = 1, button = "Left" } },
     --  mods = "NONE",
-    --  action = wezterm.action.Nop,
+    --  action = act.Nop,
     --},
     --{
     --  -- disable down event to avoid unexpected behavior
     --  event = { Down = { streak = 1, button = "Left" } },
     --  mods = "CTRL",
-    --  action = wezterm.action.Nop,
+    --  action = act.Nop,
     --},
     {
       -- CTRL-click to open hyperlinks
       event = { Up = { streak = 1, button = "Left" } },
       mods = "CTRL",
-      action = wezterm.action.OpenLinkAtMouseCursor,
+      action = act.OpenLinkAtMouseCursor,
     },
     {
       -- disable down event to avoid unexpected behavior
       event = { Down = { streak = 1, button = "Left" } },
       mods = "CTRL",
-      action = wezterm.action.Nop,
+      action = act.Nop,
     }
   }
 }
