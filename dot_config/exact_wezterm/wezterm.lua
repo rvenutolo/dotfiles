32b5f35d2190b9cc8d40ba202b5dd664520e7499
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-07-05 22:29:35 -0500

    keybindings!

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -3,6 +3,7 @@
 local wezterm = require("wezterm")
 local format_functions = require("format_functions")
 local mouse = require("mouse")
+local keybindings = require("keybindings")
 
 wezterm.on("format-tab-title", format_functions.format_tab_tile)
 wezterm.on("format-window-title", format_functions.format_window_title)
@@ -38,6 +39,8 @@ config.inactive_pane_hsb = {
 config.initial_cols = 120
 
 config.initial_rows = 36
+
+config.keys = keybindings.keys
 
 config.mouse_bindings = mouse.bindings
 
