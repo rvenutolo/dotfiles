68d2572a3bb3c7e271f6ad360de1b7f08d4d3414
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-10-22 00:36:08 -0400

    set .desktop files +x

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -8,4 +8,11 @@
   programs.home-manager.enable = true;
   targets.genericLinux.enable = true;
   xdg.mime.enable = true;
+  home.activation = {
+    linkDesktopApplications = {
+      after = ["writeBoundary" "createXdgUserDirectories" ];
+      before = [];
+      data = "find $HOME/.nix-profile/share/applications/ -iname '*.desktop' | xargs -n 1 readlink | xargs chmod +x";
+    };
+  };
 }
