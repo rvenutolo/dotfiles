5aed1e2ab46afd0bd3061da661256943e0d480f2
Rick Venutolo <rick@venutolo.org>
2024-10-22 15:19:58 -0400

    fix wrong word

--- .chezmoiscripts/run_once_after_99-change-dotfiles-git-repo-url.sh
+++ .chezmoiscripts/run_once_after_99-change-dotfiles-git-repo-url.sh
@@ -15,4 +15,4 @@ source "${HOME}/.profile"
 readonly dotfile_git_url='git@github.com:rvenutolo/dotfiles'
 log "Setting dotfiles URL to: ${dotfile_git_url}"
 git -C "${CODE_DIR}/Personal/dotfiles" remote set-url origin "${dotfile_git_url}"
-log "Setting dotfiles URL to: ${dotfile_git_url}"
+log "Set dotfiles URL to: ${dotfile_git_url}"
