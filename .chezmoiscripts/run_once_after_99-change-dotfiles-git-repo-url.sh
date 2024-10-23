3c23189768a5c2231922d96c24e46a7e98192cad
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-10-23 16:23:51 -0400

    remove unused function

--- .chezmoiscripts/run_once_after_99-change-dotfiles-git-repo-url.sh
+++ .chezmoiscripts/run_once_after_99-change-dotfiles-git-repo-url.sh
@@ -6,11 +6,6 @@ function log() {
   echo -e "\033[0;32m[$(date +%T) ${0##*/}] $*\033[0m" >&2
 }
 
-function die() {
-  echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
-  exit 1
-}
-
 source "${HOME}/.profile"
 readonly dotfile_git_url='git@github.com:rvenutolo/dotfiles'
 log "Setting dotfiles URL to: ${dotfile_git_url}"
