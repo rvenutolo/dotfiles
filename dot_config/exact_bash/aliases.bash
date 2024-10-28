44822fb28d3c8d162304b59b109cbc68aec5440e
Rick Venutolo <rick@venutolo.org>
2024-10-27 23:03:39 -0400

    add 'dcr'

--- dot_config/exact_bash/aliases.bash
+++ dot_config/exact_bash/aliases.bash
@@ -132,6 +132,7 @@ if __executable_exists 'docker'; then
   alias dcu='docker-compose-up'
   alias dcd='docker-compose-down'
   alias dcp='docker-compose-pull'
+  alias dcr='docker-compose-restart'
 fi
 
 # misc - shorter alias
