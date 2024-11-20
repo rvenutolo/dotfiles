b3e69fb4e5fde9b49f207e0a93f76d2929553be8
Rick Venutolo <rvenutolo@digitalenvoy.net>
2024-11-20 12:33:22 -0500

    add missing func

--- .chezmoiscripts/run_before_50-sdkman-config-file.sh
+++ .chezmoiscripts/run_before_50-sdkman-config-file.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+function log() {
+  echo -e "\033[0;32m[$(date +%T) ${0##*/}] $*\033[0m" >&2
+}
+
 function die() {
   echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
   exit 1
