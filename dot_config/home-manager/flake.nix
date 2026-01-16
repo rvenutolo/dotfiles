8eac5a300f9e76c021fa2823348688ee2650be75
Rick Venutolo <rick@venutolo.org>
2026-01-15 23:20:02 -0500

    nixpkgs 24.05

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
